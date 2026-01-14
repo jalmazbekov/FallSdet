97956505eb50fcb14cf36f7fcbb13b243fd16649
jalmazbekov <hazim.almazbekov@gmail.com>
2026-01-14 22:34:37 +0600

    новое смс

--- src/_25_lamda/task4/SakomotoDays.java
+++ src/_25_lamda/task4/SakomotoDays.java
@@ -4,6 +4,10 @@ import java.util.function.Predicate;
 
 public class SakomotoDays {
     public static void main(String[] args) {
-        Predicate <String> 
+
+        Predicate  <String> predicate = str -> str.equals("sako");
+        System.out.println(predicate.test("sako"));
+
+
     }
 }
